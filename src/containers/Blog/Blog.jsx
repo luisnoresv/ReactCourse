037dbae7b3dc740065d86563b6e2840b5c6e6325
Luis Alberto Nores Villaverde <luisnoresv@outlook.com>
2019-04-25 02:00:11 -0500

    Adding routing, fake backend services

---
+++ src/containers/Blog/Blog.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Route, Link, NavLink } from "react-router-dom";
+
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+import NewPost from "../Blog/NewPost/NewPost";
+import FullPost from "../Blog/FullPost/FullPost";
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline"
+                  }}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true"
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
+        <Route path="/:id" exact component={FullPost} />
+      </div>
+    );
+  }
+}
+
+export default Blog;
